09b7e6ad144d3aa3b1a38118ceb47bef44e0f4aa
aniketagnihotri <54214125+aniketagnihotri@users.noreply.github.com>
2020-07-10 22:48:56 -0400

    improvement of front-end

---
+++ src/main/js/frontend/src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return <div><h1>hello there my friend!</h1>, <button>sample</button></div>;
+    }
+}
+
+export default Counter;
