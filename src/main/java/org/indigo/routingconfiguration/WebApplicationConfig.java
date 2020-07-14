b7ba20a3d02d39cd493b894021701ffb16d5c504
aniketagnihotri <54214125+aniketagnihotri@users.noreply.github.com>
2020-07-14 03:27:35 -0400

    fix of node_modules

--- src/main/java/org/indigo/routingconfiguration/WebApplicationConfig.java
+++ src/main/java/org/indigo/routingconfiguration/WebApplicationConfig.java
@@ -1,4 +1,4 @@
-package org.indigo;
+package org.indigo.routingconfiguration;
 
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
@@ -8,15 +8,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/*
+ * This class adds a routing configuration to avoid routing conflicts
+ * between Spring Boot and React.
+ */
 @Configuration
 public class WebApplicationConfig extends WebMvcConfigurerAdapter {
 
+    /*
+     * addViewControllers forwards any pages that redirect to "/notFound" to index.html, which
+     * manages the routing for React.
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/notFound").setViewName("forward:/index.html");
     }
 
-
+    /*
+     * containerCustomizer defines "/notFound" as an error page, which is redirected to.
+     */
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
         return container -> {
