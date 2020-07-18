d73414f2f76aa6c57261060b294f5a4a63087211
aniketagnihotri <54214125+aniketagnihotri@users.noreply.github.com>
2020-07-17 23:50:56 -0400

    inclusion of https support

--- src/main/java/org/indigo/routingconfiguration/WebSecurityConfig.java
+++ src/main/java/org/indigo/routingconfiguration/WebSecurityConfig.java
@@ -1,20 +1,17 @@
 package org.indigo.routingconfiguration;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-public class WebSecurityConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.requiresChannel()
+//                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//                .requiresSecure();
+//    }
 
 }
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////    @Override
-////    protected void configure(HttpSecurity http) throws Exception {
-////        http.requiresChannel()
-////                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-////                .requiresSecure();
-////    }
-//
-//}
