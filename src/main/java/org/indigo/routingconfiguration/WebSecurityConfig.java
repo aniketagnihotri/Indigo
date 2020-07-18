a3e4b313caf0885c94020e149ff65f8a49ded767
aniketagnihotri <54214125+aniketagnihotri@users.noreply.github.com>
2020-07-18 02:36:54 -0400

    manage of https security implementation

--- src/main/java/org/indigo/routingconfiguration/WebSecurityConfig.java
+++ src/main/java/org/indigo/routingconfiguration/WebSecurityConfig.java
@@ -1,19 +1,19 @@
-package org.indigo.routingconfiguration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Profile("prod")
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
-    }
-
-}
+//package org.indigo.routingconfiguration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Profile("prod")
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.requiresChannel()
+//                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//                .requiresSecure();
+//    }
+//
+//}
